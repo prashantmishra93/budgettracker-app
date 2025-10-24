a81735b872fbab2dc0ebd296499082260094b104
Prashant Mishra <84780159+prashantmishra93@users.noreply.github.com>
2025-10-24 12:09:18 +0530

    Update Main.js
    check user get or not

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -2,12 +2,21 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { getUser } from '../redux/userSlice/getUser';
 import RouterModel from '../SubComponent/RouterModel';
+import Loading from '../Common/Loading';
 
 const Main = () => {
     const dispatch = useDispatch();
+    const user = useSelector((state) => state.userSlice.user);  // Get user details from Redux store
+    
     useEffect(() => {
         dispatch(getUser());
     }, [])
+    
+  // Display a loading state until user details are fetched
+    if (!user) {
+        return <Loading />;
+    }
+    
     return (
         <div>
             <RouterModel />
