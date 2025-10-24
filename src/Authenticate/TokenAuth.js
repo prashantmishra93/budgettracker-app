7180aa0d7ce9dee3a72e183d6bbbdfe6bc289a34
Prashant Mishra <84780159+prashantmishra93@users.noreply.github.com>
2025-10-24 12:10:52 +0530

    Update TokenAuth.js
    change authenticated check url

--- src/Authenticate/TokenAuth.js
+++ src/Authenticate/TokenAuth.js
@@ -2,7 +2,7 @@ import { Navigate } from 'react-router-dom';
 const TokenAuth = ({ component: Component }) => {
     const token = localStorage.getItem('token');
     if (!token) {
-        return <Navigate to="/login" replace />
+        return <Navigate to="/" replace />
     }
     return <Component />;
 };
